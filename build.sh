5fb0db4408f37c75a67ae43c651e0528be766243
mike castleman <m@mlcastle.net>
2018-11-10 22:25:44 -0500

    move build instructions to package.json

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ set -ex
 
 rm -rf dist dist.zip
 mkdir dist
-yarn webpack --config webpack.config.js --mode production
+yarn build
 zip -j -9 -r dist.zip dist
 aws lambda update-function-code --function-name 'arn:aws:lambda:us-east-1:859317109141:function:sixdollargas-update' --zip-file fileb://dist.zip
