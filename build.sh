e6fa061d1ebd5353578bbe3ec2d0d3d1a8ad724c
mike castleman <m@mlcastle.net>
2021-02-16 16:50:06 -0500

    disable AWS_PAGER

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ yarn build
 zip -j -9 -r dist.zip dist
 
 fn="gas-lambda/dist-$(date +%s).zip"
+export AWS_PAGER=""
 aws s3 cp dist.zip "s3://cf-logs-mlc/$fn"
 aws lambda update-function-code --function-name 'arn:aws:lambda:us-east-1:859317109141:function:sixdollargas-update' --s3-bucket cf-logs-mlc --s3-key "$fn"
 aws lambda update-function-code --function-name 'arn:aws:lambda:us-east-1:859317109141:function:sixdollargas-stats' --s3-bucket cf-logs-mlc --s3-key "$fn"
