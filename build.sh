96117b8310016d4f54839885dd033b4f62210e7b
mike castleman <m@mlcastle.net>
2021-02-16 02:22:14 -0500

    s3 for lambda update

--- build.sh
+++ build.sh
@@ -6,6 +6,9 @@ rm -rf dist dist.zip
 mkdir dist
 yarn build
 zip -j -9 -r dist.zip dist
-aws lambda update-function-code --function-name 'arn:aws:lambda:us-east-1:859317109141:function:sixdollargas-update' --zip-file fileb://dist.zip
-aws lambda update-function-code --function-name 'arn:aws:lambda:us-east-1:859317109141:function:sixdollargas-stats' --zip-file fileb://dist.zip
+
+fn="gas-lambda/dist-$(date +%s).zip"
+aws s3 cp dist.zip "s3://cf-logs-mlc/$fn"
+aws lambda update-function-code --function-name 'arn:aws:lambda:us-east-1:859317109141:function:sixdollargas-update' --s3-bucket cf-logs-mlc --s3-key "$fn"
+aws lambda update-function-code --function-name 'arn:aws:lambda:us-east-1:859317109141:function:sixdollargas-stats' --s3-bucket cf-logs-mlc --s3-key "$fn"
 aws s3 cp pages/stats.html s3://sixdollargas.org/ --cache-control 'public,max-age=3600' --content-type 'text/html;charset=utf-8'
