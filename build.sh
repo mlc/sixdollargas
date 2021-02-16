b1efdb81f3ea752d76de0d60d5413b9d28838c87
mike castleman <m@mlcastle.net>
2021-02-16 02:00:56 -0500

    stats page

--- build.sh
+++ build.sh
@@ -8,3 +8,4 @@ yarn build
 zip -j -9 -r dist.zip dist
 aws lambda update-function-code --function-name 'arn:aws:lambda:us-east-1:859317109141:function:sixdollargas-update' --zip-file fileb://dist.zip
 aws lambda update-function-code --function-name 'arn:aws:lambda:us-east-1:859317109141:function:sixdollargas-stats' --zip-file fileb://dist.zip
+aws s3 cp pages/stats.html s3://sixdollargas.org/ --cache-control 'public,max-age=3600' --content-type 'text/html;charset=utf-8'
