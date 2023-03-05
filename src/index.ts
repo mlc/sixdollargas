49f576eadf5d73c7e027d06400e3eb9e25af755a
mike castleman <m@mlcastle.net>
2023-03-05 02:27:11 -0500

    whatever

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-import 'source-map-support/register';
+import sourceMapSupport from 'source-map-support';
 import '@js-joda/timezone/dist/js-joda-timezone-10-year-range';
+
+sourceMapSupport.install();
 
 export { default as handler } from './update';
 export { default as getStats } from './stats';
