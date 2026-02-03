5f04194e0c4af8a11a478a6ec32c86e4e1f82728
mike castleman <m@mlcastle.net>
2026-02-02 23:25:19 -0500

    done w nodejs upgrade

--- build.sh
+++ build.sh
@@ -10,8 +10,8 @@ zip -j -9 -r dist.zip dist
 fn="gas-lambda/dist-$(date +%s).zip"
 export AWS_PAGER=""
 aws s3 cp dist.zip "s3://cf-logs-mlc/$fn"
-aws lambda update-function-configuration --function-name 'arn:aws:lambda:us-east-1:859317109141:function:sixdollargas-update' --runtime nodejs24.x
-aws lambda update-function-configuration --function-name 'arn:aws:lambda:us-east-1:859317109141:function:sixdollargas-stats' --runtime nodejs24.x
+#aws lambda update-function-configuration --function-name 'arn:aws:lambda:us-east-1:859317109141:function:sixdollargas-update' --runtime nodejs24.x
+#aws lambda update-function-configuration --function-name 'arn:aws:lambda:us-east-1:859317109141:function:sixdollargas-stats' --runtime nodejs24.x
 aws lambda update-function-code --function-name 'arn:aws:lambda:us-east-1:859317109141:function:sixdollargas-update' --s3-bucket cf-logs-mlc --s3-key "$fn"
 aws lambda update-function-code --function-name 'arn:aws:lambda:us-east-1:859317109141:function:sixdollargas-stats' --s3-bucket cf-logs-mlc --s3-key "$fn"
 aws s3 cp pages/stats.html s3://sixdollargas.org/ --cache-control 'public,max-age=3600' --content-type 'text/html;charset=utf-8'
